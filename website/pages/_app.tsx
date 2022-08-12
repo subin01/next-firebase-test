ff053222d551087bf7dc7885732b9849dfd59f6f
Subin Paul <subin01@gmail.com>
2022-08-12 20:47:39 +1000

    env import

--- website/pages/_app.tsx
+++ website/pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import {ENV_TYPE} from 'env'
+import {ENV_TYPE} from '../env.js'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (<><h2>ENV_TYPE: {ENV_TYPE}</h2><Component {...pageProps} /></>)
