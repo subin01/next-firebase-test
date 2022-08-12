f5251c3cffe056db003394d8dbff6ea8fe0f5aef
Subin Paul <subin01@gmail.com>
2022-08-12 19:50:07 +1000

    Env switch

--- website/pages/_app.tsx
+++ website/pages/_app.tsx
@@ -1,8 +1,9 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import {ENV_TYPE} from 'env'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (<><h2>ENV_TYPE: {ENV_TYPE}</h2><Component {...pageProps} /></>)
 }
 
 export default MyApp
